41be0e6c41760fb6d600a4116f8e1415a3e04f3e
Antti Norrkniivilä <antti.norrkniivila@aalto.fi>
2022-02-01 16:55:36 +0200

    nvm

--- src/App.js
+++ src/App.js
@@ -7,13 +7,11 @@ import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Footer from './components/parts/Footer'
 
-const history = useRouterHistory(createHistory)({
-  basename: '/projectFolder'
-})
+
 
 const App = () => {
   return (
-    <Router history={history}>
+    <Router>
       <div className="App">
         
         <Routes>
